9a599631e596003bfa81884e138788d25ba98e56
yaroslav Korzg <korzg3212@gmail.com>
2021-11-07 16:48:59 +0300

    точно Окончатетельная версия my_true_cipher

--- tasks.py
+++ tasks.py
@@ -1,32 +1,19 @@
 import string
 
-from main import search_shifted
-
 
 def my_cipher():
-    alphabet = string.ascii_lowercase
-    input_str = input()
-    result = ""
-    for i in input_str:
-        found_index = alphabet.find(i)
-        result += alphabet[found_index + 1]
-    print(result)
-
-
-def my_search():
-    stri = input()
-    el = input()
-
-    for i, value in enumerate(stri):
-        if value == el:
-            print(i)
-            break
-
-
-def my_true_cipher():
     input_str = input()
     result = ""
     for i in input_str:
         found_index = search_shifted(i)
         result += found_index
     print(result)
+
+
+def search_shifted(char):
+    alphabet = string.ascii_lowercase + "a"
+    index = alphabet.find(char)
+    return alphabet[index + 1]
+
+
+print(search_shifted("a"))
